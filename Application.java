5ebf3724bb201e070840e4215f95835381a75f5b
yamini030701 <yaminipriyalakshmi@gmail.com>
2024-05-27 12:10:21 +0530

    feature505 changes in application.java

--- Application.java
+++ Application.java
@@ -22,3 +22,21 @@ class Main {
     obj.display2(24);
   }
 }
+
+#feature505 changes by ana on may-2024
+public class Main {
+
+  // method defined
+  private static int getSquare(int x){
+    return x * x;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+
+      // method call
+      int result = getSquare(i);
+      System.out.println("Square of " + i + " is: " + result);
+    }
+  }
+}
